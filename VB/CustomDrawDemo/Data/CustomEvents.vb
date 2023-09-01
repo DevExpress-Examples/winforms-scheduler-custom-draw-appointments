931383714a527faf55211cd997e676dcc9f51af2
DevExpressExampleBot <fakeEmail>
2023-09-01 13:02:08 +0400

    Source auto update [skip ci]

--- VB/CustomDrawDemo/Data/CustomEvents.vb
+++ VB/CustomDrawDemo/Data/CustomEvents.vb
@@ -1,251 +1,314 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections
 Imports System.ComponentModel
 
 Namespace CustomDrawDemo
 
-	Public Class CustomEvent
-		Implements IEditableObject
-		Private fStart As DateTime
-		Private fEnd As DateTime
-		Private fSubject As String
-		Private fStatus As Integer
-		Private fDescription As String
-		Private fLabel As Long
-		Private fLocation As String
-		Private fAllday As Boolean
-		Private fEventType As Integer
-		Private fRecurrenceInfo As String
-		Private fReminderInfo As String
-		Private fOwnerId As Object
+    Public Class CustomEvent
+        Implements IEditableObject
 
-		Private events As CustomEventList
-		Private committed As Boolean = False
+        Private fStart As Date
 
-		Public Sub New(ByVal events As CustomEventList)
-			Me.events = events
-		End Sub
+        Private fEnd As Date
 
-		Private Sub OnListChanged()
-			Dim index As Integer = events.IndexOf(Me)
-			events.OnListChanged(New ListChangedEventArgs(ListChangedType.ItemChanged, index))
-		End Sub
+        Private fSubject As String
 
-		Public Property StartTime() As DateTime
-			Get
-				Return fStart
-			End Get
-			Set(ByVal value As DateTime)
-				fStart = value
-			End Set
-		End Property
-		Public Property EndTime() As DateTime
-			Get
-				Return fEnd
-			End Get
-			Set(ByVal value As DateTime)
-				fEnd = value
-			End Set
-		End Property
-		Public Property Subject() As String
-			Get
-				Return fSubject
-			End Get
-			Set(ByVal value As String)
-				fSubject = value
-			End Set
-		End Property
-		Public Property Status() As Integer
-			Get
-				Return fStatus
-			End Get
-			Set(ByVal value As Integer)
-				fStatus = value
-			End Set
-		End Property
-		Public Property Description() As String
-			Get
-				Return fDescription
-			End Get
-			Set(ByVal value As String)
-				fDescription = value
-			End Set
-		End Property
-		Public Property Label() As Long
-			Get
-				Return fLabel
-			End Get
-			Set(ByVal value As Long)
-				fLabel = value
-			End Set
-		End Property
-		Public Property Location() As String
-			Get
-				Return fLocation
-			End Get
-			Set(ByVal value As String)
-				fLocation = value
-			End Set
-		End Property
-		Public Property AllDay() As Boolean
-			Get
-				Return fAllday
-			End Get
-			Set(ByVal value As Boolean)
-				fAllday = value
-			End Set
-		End Property
-		Public Property EventType() As Integer
-			Get
-				Return fEventType
-			End Get
-			Set(ByVal value As Integer)
-				fEventType = value
-			End Set
-		End Property
-		Public Property RecurrenceInfo() As String
-			Get
-				Return fRecurrenceInfo
-			End Get
-			Set(ByVal value As String)
-				fRecurrenceInfo = value
-			End Set
-		End Property
-		Public Property ReminderInfo() As String
-			Get
-				Return fReminderInfo
-			End Get
-			Set(ByVal value As String)
-				fReminderInfo = value
-			End Set
-		End Property
-		Public Property OwnerId() As Object
-			Get
-				Return fOwnerId
-			End Get
-			Set(ByVal value As Object)
-				fOwnerId = value
-			End Set
-		End Property
+        Private fStatus As Integer
 
-		Public Sub BeginEdit() Implements IEditableObject.BeginEdit
-		End Sub
-		Public Sub CancelEdit() Implements IEditableObject.CancelEdit
-			If (Not committed) Then
-				CType(events, IList).Remove(Me)
-			End If
-		End Sub
-		Public Sub EndEdit() Implements IEditableObject.EndEdit
-			committed = True
-		End Sub
-	End Class
+        Private fDescription As String
 
-	Public Class CustomEventList
-		Inherits CollectionBase
-		Implements IBindingList
-		Default Public ReadOnly Property Item(ByVal idx As Integer) As CustomEvent
-			Get
-				Return CType(MyBase.List(idx), CustomEvent)
-			End Get
-		End Property
+        Private fLabel As Long
 
-		Public Shadows Sub Clear() Implements System.Collections.IList.Clear
-			MyBase.Clear()
-			OnListChanged(New ListChangedEventArgs(ListChangedType.Reset, -1))
-		End Sub
-		Public Sub Add(ByVal appointment As CustomEvent)
-			MyBase.List.Add(appointment)
-		End Sub
-		Public Function IndexOf(ByVal appointment As CustomEvent) As Integer
-			Return List.IndexOf(appointment)
-		End Function
-		Public Function AddNew() As Object Implements IBindingList.AddNew
-			Dim app As New CustomEvent(Me)
-			List.Add(app)
-			Return app
-		End Function
-		Public ReadOnly Property AllowEdit() As Boolean Implements IBindingList.AllowEdit
-			Get
-				Return True
-			End Get
-		End Property
-		Public ReadOnly Property AllowNew() As Boolean Implements IBindingList.AllowNew
-			Get
-				Return True
-			End Get
-		End Property
-		Public ReadOnly Property AllowRemove() As Boolean Implements IBindingList.AllowRemove
-			Get
-				Return True
-			End Get
-		End Property
+        Private fLocation As String
 
-		Private Event listChangedHandler As ListChangedEventHandler
-		Public Custom Event ListChanged As ListChangedEventHandler Implements IBindingList.ListChanged
-			AddHandler(ByVal value As ListChangedEventHandler)
-				AddHandler listChangedHandler, value
-			End AddHandler
-			RemoveHandler(ByVal value As ListChangedEventHandler)
-				RemoveHandler listChangedHandler, value
-			End RemoveHandler
-			RaiseEvent(ByVal sender As System.Object, ByVal e As System.ComponentModel.ListChangedEventArgs)
-			End RaiseEvent
-		End Event
-		Friend Sub OnListChanged(ByVal args As ListChangedEventArgs)
-			RaiseEvent listChangedHandler(Me, args)
-		End Sub
-		Protected Overrides Sub OnRemoveComplete(ByVal index As Integer, ByVal value As Object)
-			OnListChanged(New ListChangedEventArgs(ListChangedType.ItemDeleted, index))
-		End Sub
-		Protected Overrides Sub OnInsertComplete(ByVal index As Integer, ByVal value As Object)
-			OnListChanged(New ListChangedEventArgs(ListChangedType.ItemAdded, index))
-		End Sub
+        Private fAllday As Boolean
 
-		Public Sub AddIndex(ByVal pd As PropertyDescriptor) Implements IBindingList.AddIndex
-			Throw New NotSupportedException()
-		End Sub
-		Public Sub ApplySort(ByVal pd As PropertyDescriptor, ByVal dir As ListSortDirection) Implements IBindingList.ApplySort
-			Throw New NotSupportedException()
-		End Sub
-		Public Function Find(ByVal [property] As PropertyDescriptor, ByVal key As Object) As Integer Implements IBindingList.Find
-			Throw New NotSupportedException()
-		End Function
-		Public ReadOnly Property IsSorted() As Boolean Implements IBindingList.IsSorted
-			Get
-				Return False
-			End Get
-		End Property
-		Public Sub RemoveIndex(ByVal pd As PropertyDescriptor) Implements IBindingList.RemoveIndex
-			Throw New NotSupportedException()
-		End Sub
-		Public Sub RemoveSort() Implements IBindingList.RemoveSort
-			Throw New NotSupportedException()
-		End Sub
-		Public ReadOnly Property SortDirection() As ListSortDirection Implements IBindingList.SortDirection
-			Get
-				Throw New NotSupportedException()
-			End Get
-		End Property
-		Public ReadOnly Property SortProperty() As PropertyDescriptor Implements IBindingList.SortProperty
-			Get
-				Throw New NotSupportedException()
-			End Get
-		End Property
-		Public ReadOnly Property SupportsChangeNotification() As Boolean Implements IBindingList.SupportsChangeNotification
-			Get
-				Return True
-			End Get
-		End Property
-		Public ReadOnly Property SupportsSearching() As Boolean Implements IBindingList.SupportsSearching
-			Get
-				Return False
-			End Get
-		End Property
-		Public ReadOnly Property SupportsSorting() As Boolean Implements IBindingList.SupportsSorting
-			Get
-				Return False
-			End Get
-		End Property
-	End Class
+        Private fEventType As Integer
+
+        Private fRecurrenceInfo As String
+
+        Private fReminderInfo As String
+
+        Private fOwnerId As Object
+
+        Private events As CustomEventList
+
+        Private committed As Boolean = False
+
+        Public Sub New(ByVal events As CustomEventList)
+            Me.events = events
+        End Sub
+
+        Private Sub OnListChanged()
+            Dim index As Integer = events.IndexOf(Me)
+            events.OnListChanged(New ListChangedEventArgs(ListChangedType.ItemChanged, index))
+        End Sub
+
+        Public Property StartTime As Date
+            Get
+                Return fStart
+            End Get
+
+            Set(ByVal value As Date)
+                fStart = value
+            End Set
+        End Property
+
+        Public Property EndTime As Date
+            Get
+                Return fEnd
+            End Get
+
+            Set(ByVal value As Date)
+                fEnd = value
+            End Set
+        End Property
+
+        Public Property Subject As String
+            Get
+                Return fSubject
+            End Get
+
+            Set(ByVal value As String)
+                fSubject = value
+            End Set
+        End Property
+
+        Public Property Status As Integer
+            Get
+                Return fStatus
+            End Get
+
+            Set(ByVal value As Integer)
+                fStatus = value
+            End Set
+        End Property
+
+        Public Property Description As String
+            Get
+                Return fDescription
+            End Get
+
+            Set(ByVal value As String)
+                fDescription = value
+            End Set
+        End Property
+
+        Public Property Label As Long
+            Get
+                Return fLabel
+            End Get
+
+            Set(ByVal value As Long)
+                fLabel = value
+            End Set
+        End Property
+
+        Public Property Location As String
+            Get
+                Return fLocation
+            End Get
+
+            Set(ByVal value As String)
+                fLocation = value
+            End Set
+        End Property
+
+        Public Property AllDay As Boolean
+            Get
+                Return fAllday
+            End Get
+
+            Set(ByVal value As Boolean)
+                fAllday = value
+            End Set
+        End Property
+
+        Public Property EventType As Integer
+            Get
+                Return fEventType
+            End Get
+
+            Set(ByVal value As Integer)
+                fEventType = value
+            End Set
+        End Property
+
+        Public Property RecurrenceInfo As String
+            Get
+                Return fRecurrenceInfo
+            End Get
+
+            Set(ByVal value As String)
+                fRecurrenceInfo = value
+            End Set
+        End Property
+
+        Public Property ReminderInfo As String
+            Get
+                Return fReminderInfo
+            End Get
+
+            Set(ByVal value As String)
+                fReminderInfo = value
+            End Set
+        End Property
+
+        Public Property OwnerId As Object
+            Get
+                Return fOwnerId
+            End Get
+
+            Set(ByVal value As Object)
+                fOwnerId = value
+            End Set
+        End Property
+
+        Public Sub BeginEdit() Implements IEditableObject.BeginEdit
+        End Sub
+
+        Public Sub CancelEdit() Implements IEditableObject.CancelEdit
+            If Not committed Then
+                CType(events, IList).Remove(Me)
+            End If
+        End Sub
+
+        Public Sub EndEdit() Implements IEditableObject.EndEdit
+            committed = True
+        End Sub
+    End Class
+
+    Public Class CustomEventList
+        Inherits CollectionBase
+        Implements IBindingList
+
+        Default Public ReadOnly Property Item(ByVal idx As Integer) As CustomEvent
+            Get
+                Return CType(List(idx), CustomEvent)
+            End Get
+        End Property
+
+        Public Overloads Sub Clear() Implements IList.Clear
+            MyBase.Clear()
+            OnListChanged(New ListChangedEventArgs(ListChangedType.Reset, -1))
+        End Sub
+
+        Public Sub Add(ByVal appointment As CustomEvent)
+            List.Add(appointment)
+        End Sub
+
+        Public Function IndexOf(ByVal appointment As CustomEvent) As Integer
+            Return List.IndexOf(appointment)
+        End Function
+
+        Public Function AddNew() As Object Implements IBindingList.AddNew
+            Dim app As CustomEvent = New CustomEvent(Me)
+            List.Add(app)
+            Return app
+        End Function
+
+        Public ReadOnly Property AllowEdit As Boolean Implements IBindingList.AllowEdit
+            Get
+                Return True
+            End Get
+        End Property
+
+        Public ReadOnly Property AllowNew As Boolean Implements IBindingList.AllowNew
+            Get
+                Return True
+            End Get
+        End Property
+
+        Public ReadOnly Property AllowRemove As Boolean Implements IBindingList.AllowRemove
+            Get
+                Return True
+            End Get
+        End Property
+
+        Private listChangedHandler As ListChangedEventHandler
+
+        Public Custom Event ListChanged As ListChangedEventHandler Implements IBindingList.ListChanged
+            AddHandler(ByVal value As ListChangedEventHandler)
+                listChangedHandler = [Delegate].Combine(listChangedHandler, value)
+            End AddHandler
+
+            RemoveHandler(ByVal value As ListChangedEventHandler)
+                listChangedHandler = [Delegate].Remove(listChangedHandler, value)
+            End RemoveHandler
+
+            RaiseEvent(ByVal sender As Object, ByVal e As ListChangedEventArgs)
+                listChangedHandler ?(sender, e)
+            End RaiseEvent
+        End Event
+
+        Friend Sub OnListChanged(ByVal args As ListChangedEventArgs)
+            If listChangedHandler IsNot Nothing Then
+                listChangedHandler(Me, args)
+            End If
+        End Sub
+
+        Protected Overrides Sub OnRemoveComplete(ByVal index As Integer, ByVal value As Object)
+            OnListChanged(New ListChangedEventArgs(ListChangedType.ItemDeleted, index))
+        End Sub
+
+        Protected Overrides Sub OnInsertComplete(ByVal index As Integer, ByVal value As Object)
+            OnListChanged(New ListChangedEventArgs(ListChangedType.ItemAdded, index))
+        End Sub
+
+        Public Sub AddIndex(ByVal pd As PropertyDescriptor) Implements IBindingList.AddIndex
+            Throw New NotSupportedException()
+        End Sub
+
+        Public Sub ApplySort(ByVal pd As PropertyDescriptor, ByVal dir As ListSortDirection) Implements IBindingList.ApplySort
+            Throw New NotSupportedException()
+        End Sub
+
+        Public Function Find(ByVal [property] As PropertyDescriptor, ByVal key As Object) As Integer Implements IBindingList.Find
+            Throw New NotSupportedException()
+        End Function
+
+        Public ReadOnly Property IsSorted As Boolean Implements IBindingList.IsSorted
+            Get
+                Return False
+            End Get
+        End Property
+
+        Public Sub RemoveIndex(ByVal pd As PropertyDescriptor) Implements IBindingList.RemoveIndex
+            Throw New NotSupportedException()
+        End Sub
+
+        Public Sub RemoveSort() Implements IBindingList.RemoveSort
+            Throw New NotSupportedException()
+        End Sub
+
+        Public ReadOnly Property SortDirection As ListSortDirection Implements IBindingList.SortDirection
+            Get
+                Throw New NotSupportedException()
+            End Get
+        End Property
+
+        Public ReadOnly Property SortProperty As PropertyDescriptor Implements IBindingList.SortProperty
+            Get
+                Throw New NotSupportedException()
+            End Get
+        End Property
+
+        Public ReadOnly Property SupportsChangeNotification As Boolean Implements IBindingList.SupportsChangeNotification
+            Get
+                Return True
+            End Get
+        End Property
+
+        Public ReadOnly Property SupportsSearching As Boolean Implements IBindingList.SupportsSearching
+            Get
+                Return False
+            End Get
+        End Property
+
+        Public ReadOnly Property SupportsSorting As Boolean Implements IBindingList.SupportsSorting
+            Get
+                Return False
+            End Get
+        End Property
+    End Class
 End Namespace
